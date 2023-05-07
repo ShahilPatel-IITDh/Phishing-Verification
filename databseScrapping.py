3c177617c34d46a8d48f8b005b226c922e6bb210
Shahil Patel <83348636+ShahilPatel-IITDh@users.noreply.github.com>
2023-05-07 16:11:13 +0530

    Updated the code to filter the URLs and remove the "added .." substring from URL

--- databseScrapping.py
+++ databseScrapping.py
@@ -2,6 +2,13 @@ import csv
 import requests
 from bs4 import BeautifulSoup
 
+# function to remove a given substring from a string
+def removeSubstr(string, sub):
+    if string.find(sub) == -1:
+        return string
+    else:
+        return string.replace(sub, "")
+
 # Create a list to store the data
 data = []
 
@@ -24,8 +31,15 @@ for page in range(15):
     # Loop through each row in the table and extract the Phish ID and Phish URL
     for row in rows[1:]:
         cells = row.find_all("td")
+        
+        # Find the span element in the row, we will use the value inside it to be removed from the URL string
+        span_element = row.find('span', class_='small')
+        substr = span_element.text.strip()
+        # print(substr)
+
         phish_id = cells[0].text.strip()
-        url = cells[1].text.strip()
+        extraURL = cells[1].text.strip()
+        url = removeSubstr(extraURL, substr)
         data.append([phish_id, url])
 
 # Write the data to a CSV file
