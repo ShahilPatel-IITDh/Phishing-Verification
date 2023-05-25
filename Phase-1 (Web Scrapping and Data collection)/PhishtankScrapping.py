1e046b47be56f96a388a0ddee95091c401912231
Shahil Patel <83348636+ShahilPatel-IITDh@users.noreply.github.com>
2023-05-26 00:06:31 +0530

    working code for full URL link using selenium automation
    The code uses a single URL ID and page URL of phishtank database, however to scale it up, we plan to iterate through each webpage and click on the URL ID on that page, also after clicking copy the URL present on that page using xPath, then put the output in csv file.

--- Phase-1 (Web Scrapping and Data collection)/PhishtankScrapping.py
+++ Phase-1 (Web Scrapping and Data collection)/PhishtankScrapping.py
@@ -6,8 +6,57 @@
 # 5. Save the URL and it's corresponding ID in a CSV file on local machine
 
 # Import the required libraries
+import csv
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import NoSuchElementException
 
+# Set up Chrome options
+chrome_options = Options()
+chrome_options.add_argument("--headless")  # Run Chrome in headless mode
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+
+# Set path to your ChromeDriver executable
+webdriver_service = Service('C:\\Users\\Admin\\Documents\\chromedriver.exe')
+
+# Launch the Chrome browser
+driver = webdriver.Chrome(service=webdriver_service, options=chrome_options)
+
+# Open the URL in Google Chrome
+url = "https://phishtank.org/phish_search.php?page=1&active=y&verified=u"
+driver.get(url)
+
+
+# URL ID should be a variable which will we iterate through each page
+URL_Id = 8155865
+
+# Check if the element with the given XPath is present
+element_xpath = f"(//a[normalize-space()={URL_Id}])[1]"
+try:
+    element = driver.find_element(By.XPATH, element_xpath)
+    # If the element is found, click on it
+    ActionChains(driver).move_to_element(element).click().perform()
+except NoSuchElementException:
+    print("Element not found on the webpage.")
+
+# Extract the text using the specified CSS selector
+text_css_selector = "/html[1]/body[1]/div[2]/div[2]/div[1]/div[1]/div[3]/span[1]/b[1]"
+try:
+    text_element = driver.find_element(By.XPATH, text_css_selector)
+    extracted_text = text_element.text
+    # Write the extracted text to a CSV file
+    output_file = "output.csv"
+    with open(output_file, "a", newline="") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([extracted_text])
+
+except NoSuchElementException:
+    print("Text element not found on the webpage.")
+
+# Close the browser
+driver.quit()
