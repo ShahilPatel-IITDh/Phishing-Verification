352db8d294afcceaf800a8ff37b517c1e8ffa2ef
Shahil Patel <83348636+ShahilPatel-IITDh@users.noreply.github.com>
2023-05-26 15:34:27 +0530

    Code generates only oen URL in csv file, need to debug the code, and UpScale it further

--- Phase-1 (Web Scrapping and Data collection)/PhishtankScrapping.py
+++ Phase-1 (Web Scrapping and Data collection)/PhishtankScrapping.py
@@ -16,9 +16,13 @@ from selenium.common.exceptions import NoSuchElementException
 
 # Set up Chrome options
 chrome_options = Options()
-chrome_options.add_argument("--headless")  # Run Chrome in headless mode
+# Run Chrome in headless mode
+chrome_options.add_argument("--headless") 
+# Disable the GPU 
 chrome_options.add_argument("--disable-gpu")
+# Disable the sandbox
 chrome_options.add_argument("--no-sandbox")
+# Disable the DevShmUsage
 chrome_options.add_argument("--disable-dev-shm-usage")
 
 # Set path to your ChromeDriver executable
@@ -27,36 +31,71 @@ webdriver_service = Service('C:\\Users\\Admin\\Documents\\chromedriver.exe')
 # Launch the Chrome browser
 driver = webdriver.Chrome(service=webdriver_service, options=chrome_options)
 
-# Open the URL in Google Chrome
-url = "https://phishtank.org/phish_search.php?page=1&active=y&verified=u"
-driver.get(url)
+for pageNo in range(2):
+
+    # Open the webpage URL in Google Chrome
+    url = f"https://phishtank.org/phish_search.php?page={pageNo}&active=y&verified=u"
+    driver.get(url)
+
+    # Iterate through each entries and get the URL IDs present on that page
+    # The URL_IDs list will store all the URLs present on the current page
+    URL_IDs = []   
+
+    for ID in range(8156400, 8156701):
+        
+        # element_xpath = f"(//a[normalize-space()={ID}])[1]"
+        element_xpath = f"//a[normalize-space()={ID}]"
+        
+        try:
+            # Check if the element with the given XPath is present
+            element = driver.find_element(By.XPATH, element_xpath)
+
+            # If the element is found, store it in the URL_IDs list, once we get a complete list, after that we will iterate through each element in the list and click on it
+            URL_IDs.append(ID)
+
+            # sleep for 5 seconds, before checking for the next element
+            # driver.implicitly_wait(5)
+            
+        except NoSuchElementException:
+            print(f"{ID} not found on the webpage no {pageNo}.")
 
 
-# URL ID should be a variable which will we iterate through each page
-URL_Id = 8155865
+    # After this loop we will get a complete list of all the URL IDs present on the current page
+    # Iterate through each element in the URL_IDs list and click on it
+    
+    for ID in URL_IDs:
+        # For each URL we will find the element by it's XPath and click on it.
+        xPath = f"(//a[normalize-space()={ID}])"
+        try:
+            element = driver.find_element(By.XPATH, xPath)
+            # click on the element
+            element.click()
 
-# Check if the element with the given XPath is present
-element_xpath = f"(//a[normalize-space()={URL_Id}])[1]"
-try:
-    element = driver.find_element(By.XPATH, element_xpath)
-    # If the element is found, click on it
-    ActionChains(driver).move_to_element(element).click().perform()
-except NoSuchElementException:
-    print("Element not found on the webpage.")
+            # Alternatively we can use ActionChains to click on the element
+            # ActionChains(driver).move_to_element(element).click().perform()
 
-# Extract the text using the specified CSS selector
-text_css_selector = "/html[1]/body[1]/div[2]/div[2]/div[1]/div[1]/div[3]/span[1]/b[1]"
-try:
-    text_element = driver.find_element(By.XPATH, text_css_selector)
-    extracted_text = text_element.text
-    # Write the extracted text to a CSV file
-    output_file = "output.csv"
-    with open(output_file, "a", newline="") as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow([extracted_text])
+        except NoSuchElementException:
+            print(f"URL_ID {ID} not clickable on the webpage no {pageNo}.")
 
-except NoSuchElementException:
-    print("Text element not found on the webpage.")
+
+        # Get the text content from the new page
+
+        # Extract the text using the specified CSS selector
+        text_Xpath = "/html[1]/body[1]/div[2]/div[2]/div[1]/div[1]/div[3]/span[1]/b[1]"
+       
+        # After clicking we will be redirected to new page so use try and except block to check if the text element is present on the new page or not, if present then extract the text and write it to a CSV file
+        try: 
+            text_element = driver.find_element(By.XPATH, text_Xpath)
+            extracted_text = text_element.text
+
+            # Write the extracted text to a CSV file
+            output_file = "output.csv"
+            with open(output_file, "a", newline="") as outputFile:
+                writer = csv.writer(outputFile)
+                writer.writerow([ID ,extracted_text])
+
+        except NoSuchElementException:
+            print(f"Text element not found for {ID} on the webpage no. {pageNo}.")
 
 # Close the browser
 driver.quit()
