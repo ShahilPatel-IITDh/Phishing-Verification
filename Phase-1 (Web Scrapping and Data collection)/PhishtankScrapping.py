42ff125cb0f14a47c08f2654d0d037cb2fbdd10e
Shahil Patel <83348636+ShahilPatel-IITDh@users.noreply.github.com>
2023-05-27 20:27:18 +0530

    Working code. Tested for 2 pages. Need to refine, upscale and document the code.

--- Phase-1 (Web Scrapping and Data collection)/PhishtankScrapping.py
+++ Phase-1 (Web Scrapping and Data collection)/PhishtankScrapping.py
@@ -1,18 +1,17 @@
-# Overview: As some URLs on the PhishTank site weren't mentioned completely on a single table, the user requires to click on the URL ID link to visit a new webpage where the complete URL will be present, So this code will automate the process of:
-# 1. Open the PhishTank site
-# 2. Iterate through each webpage and it's corresponding table entries
-# 3. Click on the URL ID link to visit a new webpage, for each table entry
-# 4. Extract the URL from the new webpage
-# 5. Save the URL and it's corresponding ID in a CSV file on local machine
-
-# Import the required libraries
 import csv
+import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
 from selenium.common.exceptions import NoSuchElementException
+# function to remove a given substring from a string
+
+# Output file
+output_file = "database.csv"
 
 # Set up Chrome options
 chrome_options = Options()
@@ -25,80 +24,83 @@ chrome_options.add_argument("--no-sandbox")
 # Disable the DevShmUsage
 chrome_options.add_argument("--disable-dev-shm-usage")
 
-# Set path to your ChromeDriver executable (For windows)
-# webdriver_service = Service('C:\\Users\\Admin\\Documents\\chromedriver.exe')
+# Set up Selenium driver
+driver = webdriver.Chrome()  
+driver.maximize_window()
 
-# For ubuntu:
-webdriver_service = Service('/home/administrator/Downloads/chromedriver_linux64/chromedriver')
-
-# Launch the Chrome browser
-driver = webdriver.Chrome(service=webdriver_service, options=chrome_options)
-
-for pageNo in range(2):
-
-    # Open the webpage URL in Google Chrome
-    url = f"https://phishtank.org/phish_search.php?page={pageNo}&active=y&verified=u"
+# Loop through all the pages
+for page in range(2):
+    # Send a GET request to the webpage and get the HTML content
+    url = f"https://phishtank.org/phish_search.php?page={page}&active=y&verified=u"
     driver.get(url)
 
-    # Iterate through each entries and get the URL IDs present on that page
-    # The URL_IDs list will store all the URLs present on the current page
-    URL_IDs = []   
+    # Wait for the table to be present on the page
+    table_present = EC.presence_of_element_located((By.CLASS_NAME, "data"))
+    WebDriverWait(driver, 10).until(table_present)
 
-    for ID in range(8156400, 8156701):
-        
-        # element_xpath = f"(//a[normalize-space()={ID}])[1]"
-        element_xpath = f"//a[normalize-space()={ID}]"
-        
-        try:
-            # Check if the element with the given XPath is present
-            element = driver.find_element(By.XPATH, element_xpath)
+    # Parse the HTML content using BeautifulSoup, driver.page_source is the HTML content of the page
+    soup = BeautifulSoup(driver.page_source, "html.parser")
 
-            # If the element is found, store it in the URL_IDs list, once we get a complete list, after that we will iterate through each element in the list and click on it
-            URL_IDs.append(ID)
+    # Find the table that contains the data
+    table = soup.find("table", {"class": "data"})
 
-            # sleep for 5 seconds, before checking for the next element
-            # driver.implicitly_wait(5)
+    # Get the rows in the table
+    rows = table.find_all("tr")
+
+    # Loop through each row in the table and extract the Phish ID and Phish URL
+    for row in rows[1:]:
+        cells = row.find_all("td")
+
+        # Find the span element in the row, we will use the value inside it to be removed from the URL string
+        span_element = row.find('span', class_='small')
+        # substr = span_element.text.strip()
+
+        # Extract the Phish ID and Phish URL
+        phish_id = cells[0].text.strip()
+        extraURL = cells[1].text.strip()
+
+        # The clicking is done only to ensure that our script is working fine, if the script is working fine then we can comment the below code
+        # Click on the link with text as the value of phish_id (this is working fine in automation)
+        # link = driver.find_element(By.LINK_TEXT, phish_id)
+        # link.click()
+
+        # # Wait for the new page to load
+        # time.sleep(2)
+
+        # As we have the phish_id, open the new page directly using it.
+
+        # To Extract the URL from new page the approach used is: as we have the phish_id, we can use it to get the URL of the newly generated page
+        # The URL of the newly generated page is: https://phishtank.org/phish_detail.php?phish_id={phish_id}, after that use the same approach as used in the previous code to extract the URL
+
+        # Send a GET request to the webpage and get the HTML content
+        url = f"https://phishtank.org/phish_detail.php?phish_id={phish_id}"
+        driver.get(url)
+
+        # Parse the HTML content using BeautifulSoup, driver.page_source is the HTML content of the page
+        newSoup = BeautifulSoup(driver.page_source, "html.parser")
+
+        spanElement = newSoup.find('span', style='word-wrap:break-word;')
+
+        if spanElement is not None:
+            requiredURL = spanElement.find('b')
+            if requiredURL is not None:
+                url = requiredURL.text.strip()
+                # If the URL is present then direct write it to the output file to avoid the exceptions of NoSuchElementException
+                with open(output_file, "a", newline="") as outputFile:
+                    writer = csv.writer(outputFile)
+                    writer.writerow([phish_id ,url]) 
+                # print url for testing it
+                print(url)
             
-        except NoSuchElementException:
-            print(f"{ID} not found on the webpage no {pageNo}.")
+            else:
+                print("<b> element not found.")
+        else:
+            print("<span> element not found.")        
 
+        # Go back to the previous page
+        driver.back()
+        # Wait for the page to load
+        time.sleep(2)
 
-    # After this loop we will get a complete list of all the URL IDs present on the current page
-    # Iterate through each element in the URL_IDs list and click on it
-    
-    for ID in URL_IDs:
-        # For each URL we will find the element by it's XPath and click on it.
-        xPath = f"(//a[normalize-space()={ID}])"
-        try:
-            element = driver.find_element(By.XPATH, xPath)
-            # click on the element
-            element.click()
-
-            # Alternatively we can use ActionChains to click on the element
-            # ActionChains(driver).move_to_element(element).click().perform()
-
-        except NoSuchElementException:
-            print(f"URL_ID {ID} not clickable on the webpage no {pageNo}.")
-
-
-        # Get the text content from the new page
-
-        # Extract the text using the specified CSS selector
-        text_Xpath = "/html[1]/body[1]/div[2]/div[2]/div[1]/div[1]/div[3]/span[1]/b[1]"
-       
-        # After clicking we will be redirected to new page so use try and except block to check if the text element is present on the new page or not, if present then extract the text and write it to a CSV file
-        try: 
-            text_element = driver.find_element(By.XPATH, text_Xpath)
-            extracted_text = text_element.text
-
-            # Write the extracted text to a CSV file
-            output_file = "output.csv"
-            with open(output_file, "a", newline="") as outputFile:
-                writer = csv.writer(outputFile)
-                writer.writerow([ID ,extracted_text])
-
-        except NoSuchElementException:
-            print(f"Text element not found for {ID} on the webpage no. {pageNo}.")
-
-# Close the browser
+# Close the Selenium driver
 driver.quit()
