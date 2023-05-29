c65bba9886d1d8359cedeeb831f4d1019b8a85b6
Shahil Patel <83348636+ShahilPatel-IITDh@users.noreply.github.com>
2023-05-29 23:01:27 +0530

    Code updated to automatically iterate through all the URLs present of phishtank archives site.

--- Phase-1 (Web Scrapping and Data collection)/WebResources/extractionTrial.py
+++ Phase-1 (Web Scrapping and Data collection)/WebResources/extractionTrial.py
@@ -1,9 +1,14 @@
 import os
 import requests
+import time
 from selenium import webdriver
-
-# set the URL of the webpage to download
-url = "https://facebook.com"
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+from selenium.common.exceptions import NoSuchElementException
 
 # set the path to the Chrome driver executable (for ubuntu)
 chromedriver_path = "/home/administrator/Downloads/chromedriver_linux64/chromedriver"
@@ -11,44 +16,60 @@ chromedriver_path = "/home/administrator/Downloads/chromedriver_linux64/chromedr
 # create a new Chrome browser instance
 options = webdriver.ChromeOptions()
 options.add_argument("--headless")
-browser = webdriver.Chrome(executable_path=chromedriver_path, options=options)
+options.add_argument("--disable-gpu")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
 
-# navigate to the URL
-browser.get(url)
+browser = webdriver.Chrome(service=Service(executable_path=chromedriver_path), options=options)
+browser.maximize_window()
 
-# get the source page of the current
-source = browser.page_source
-with open("source.html", "w") as f:
-    f.write(source)
+# loop through all the pages
+for pageNo in range(2):
+    url = f"https://phishtank.org/phish_search.php?page={pageNo}&active=y&valid=y&Search=Search"
+    # navigate to the URL
+    browser.get(url)
+    # Wait for the table to be present on the page
+    table_present = EC.presence_of_element_located((By.CLASS_NAME, "data"))
+    WebDriverWait(browser, 10).until(table_present)
 
-# get the URLs of all the JS scripts on the page
-js_urls = []
-for script in browser.find_elements_by_tag_name("script"):
-    src = script.get_attribute("src")
-    if src:
-        js_urls.append(src)
+    # Parse the HTML content using BeautifulSoup, driver.page_source is the HTML content of the page
+    soup = BeautifulSoup(browser.page_source, "html.parser")
 
-# download each JS script
-for js_url in js_urls:
-    response = requests.get(js_url)
-    filename = os.path.basename(js_url)
-    with open(filename, "wb") as f:
-        f.write(response.content)
+    # Find the table that contains the data
+    table = soup.find("table", {"class": "data"})
 
-# get the URLs of all the CSS files on the page
-css_urls = []
-for link in browser.find_elements_by_tag_name("link"):
-    rel = link.get_attribute("rel")
-    if rel == "stylesheet":
-        href = link.get_attribute("href")
-        css_urls.append(href)
+    # Get the rows in the table
+    rows = table.find_all("tr")
 
-# download each CSS file
-for css_url in css_urls:
-    response = requests.get(css_url)
-    filename = os.path.basename(css_url)
-    with open(filename, "wb") as f:
-        f.write(response.content)
+    # Loop through each row in the table and extract the Phish ID and Phish URL
+    for row in rows[1:]:
+        cells = row.find_all("td")
+
+        # Find the span element in the row, we will use the value inside it to be removed from the URL string
+        span_element = row.find('span', class_='small')
+        # substr = span_element.text.strip()
+
+        # Extract the Phish ID and Phish URL
+        phish_id = cells[0].text.strip()
+        extraURL = cells[1].text.strip()
+
+        # Send a GET request to the webpage and get the HTML content
+        url = f"https://phishtank.org/phish_detail.php?phish_id={phish_id}"
+        browser.get(url)
+
+        # Parse the HTML content using BeautifulSoup, driver.page_source is the HTML content of the page
+        source = browser.page_source
+
+        # Generate a unique file name based on timestamp
+        timestamp = int(time.time())
+        file_name = f"phishyURLs/{phish_id}_{timestamp}.html"
+
+        # Create the directory if it doesn't exist
+        os.makedirs(os.path.dirname(file_name), exist_ok=True)
+
+        # Save the HTML content in the generated file
+        with open(file_name, "w") as f:
+            f.write(source)
 
 # close the browser
 browser.quit()
