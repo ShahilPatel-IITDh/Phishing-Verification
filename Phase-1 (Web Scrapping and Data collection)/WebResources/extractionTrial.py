86f9ab31752b6bff4bc844ebcef3327f960e72e3
Shahil Patel <83348636+ShahilPatel-IITDh@users.noreply.github.com>
2023-05-29 23:11:57 +0530

    Code commented

--- Phase-1 (Web Scrapping and Data collection)/WebResources/extractionTrial.py
+++ Phase-1 (Web Scrapping and Data collection)/WebResources/extractionTrial.py
@@ -9,25 +9,33 @@ from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 
 # set the path to the Chrome driver executable (for ubuntu)
-chromedriver_path = "/home/administrator/Downloads/chromedriver_linux64/chromedriver"
+driverPath = "/home/administrator/Downloads/chromedriver_linux64/chromedriver"
 
 # create a new Chrome browser instance
 options = webdriver.ChromeOptions()
+# headless mode: run Chrome in the background
 options.add_argument("--headless")
+# disable-gpu: disable the GPU hardware acceleration
 options.add_argument("--disable-gpu")
+# no-sandbox: disable the Chrome sandbox
 options.add_argument("--no-sandbox")
+# disable-dev-shm-usage: disable the /dev/shm usage
 options.add_argument("--disable-dev-shm-usage")
 
-browser = webdriver.Chrome(service=Service(executable_path=chromedriver_path), options=options)
+# create a new Chrome browser instance
+browser = webdriver.Chrome(service=Service(executable_path=driverPath), options=options)
+# maximize the browser window
 browser.maximize_window()
 
-# loop through all the pages
+# loop through all the pages, testing for 2 pages
 for pageNo in range(2):
+    # generate the URL
     url = f"https://phishtank.org/phish_search.php?page={pageNo}&active=y&valid=y&Search=Search"
     # navigate to the URL
     browser.get(url)
     # Wait for the table to be present on the page
     table_present = EC.presence_of_element_located((By.CLASS_NAME, "data"))
+    # Wait for the table to be present on the page for 10 seconds
     WebDriverWait(browser, 10).until(table_present)
 
     # Parse the HTML content using BeautifulSoup, driver.page_source is the HTML content of the page
@@ -41,6 +49,7 @@ for pageNo in range(2):
 
     # Loop through each row in the table and extract the Phish ID and Phish URL
     for row in rows[1:]:
+        # Get the cells in the row
         cells = row.find_all("td")
 
         # Find the span element in the row, we will use the value inside it to be removed from the URL string
@@ -59,8 +68,9 @@ for pageNo in range(2):
         source = browser.page_source
 
         # Generate a unique file name based on timestamp
-        timestamp = int(time.time())
-        file_name = f"phishyURLs/{phish_id}_{timestamp}.html"
+        # timestamp = int(time.time())
+        # The HTML files will be downloaded in the phishyURLs directorys
+        file_name = f"phishyURLs/{phish_id}.html"
 
         # Create the directory if it doesn't exist
         os.makedirs(os.path.dirname(file_name), exist_ok=True)
