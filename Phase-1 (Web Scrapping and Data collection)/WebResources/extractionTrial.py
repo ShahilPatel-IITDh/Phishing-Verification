94eaddb9ed618b9d43b9700072e6a332dd5e436c
Shahil Patel <83348636+ShahilPatel-IITDh@users.noreply.github.com>
2023-05-29 15:34:29 +0530

    Code for web-resource extraction, work only for HTML source code

--- Phase-1 (Web Scrapping and Data collection)/WebResources/extractionTrial.py
+++ Phase-1 (Web Scrapping and Data collection)/WebResources/extractionTrial.py
@@ -1,17 +1,54 @@
+import os
+import requests
 from selenium import webdriver
 
-# Create a new instance of the Chrome web driver
-driver = webdriver.Chrome()
+# set the URL of the webpage to download
+url = "https://facebook.com"
 
-# Navigate to the URL
-driver.get("https://wonderful-licorice-edcaea.netlify.app/")
+# set the path to the Chrome driver executable (for ubuntu)
+chromedriver_path = "/home/administrator/Downloads/chromedriver_linux64/chromedriver"
 
-# Get the HTML source code
-html_source = driver.page_source
+# create a new Chrome browser instance
+options = webdriver.ChromeOptions()
+options.add_argument("--headless")
+browser = webdriver.Chrome(executable_path=chromedriver_path, options=options)
 
-# Save the HTML source code to a local file
-with open("wonderful-licorice-edcaea.netlify.app.html", "w") as f:
-    f.write(html_source)
+# navigate to the URL
+browser.get(url)
 
-# Close the web driver
-driver.close()
+# get the source page of the current
+source = browser.page_source
+with open("source.html", "w") as f:
+    f.write(source)
+
+# get the URLs of all the JS scripts on the page
+js_urls = []
+for script in browser.find_elements_by_tag_name("script"):
+    src = script.get_attribute("src")
+    if src:
+        js_urls.append(src)
+
+# download each JS script
+for js_url in js_urls:
+    response = requests.get(js_url)
+    filename = os.path.basename(js_url)
+    with open(filename, "wb") as f:
+        f.write(response.content)
+
+# get the URLs of all the CSS files on the page
+css_urls = []
+for link in browser.find_elements_by_tag_name("link"):
+    rel = link.get_attribute("rel")
+    if rel == "stylesheet":
+        href = link.get_attribute("href")
+        css_urls.append(href)
+
+# download each CSS file
+for css_url in css_urls:
+    response = requests.get(css_url)
+    filename = os.path.basename(css_url)
+    with open(filename, "wb") as f:
+        f.write(response.content)
+
+# close the browser
+browser.quit()
