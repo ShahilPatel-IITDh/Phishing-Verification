cbadfd5a6bf63d543ce71a7b1780625fed51b4a0
Shahil Patel <83348636+ShahilPatel-IITDh@users.noreply.github.com>
2023-06-02 13:50:29 +0530

    The code extracts the page code along with styling. But works onlly for few webpages which match the code template.

--- Phase-1 (Web Scrapping and Data collection)/WebResources/F2-Automation.py
+++ Phase-1 (Web Scrapping and Data collection)/WebResources/F2-Automation.py
@@ -17,26 +17,31 @@ def download_file(url, output_dir):
         with open(filename, 'wb') as file:
             file.write(response.content)
         return filename
+    elif response.status_code == 404:
+        print(f"File not found: {url}")
+
+    else:
+        print(f"Error downloading file: {url}")
 
 def save_links_to_file(links, output_file):
     with open(output_file, 'w') as file:
         file.write('\n'.join(links))
 
-# driver path (chrome driver), in ubuntu
+# Set the path to the Chrome driver
 driverPath = "/home/administrator/Downloads/chromedriver_linux64/chromedriver"
 
-# create a new Chrome browser instance
+# Create a new Chrome browser instance
 options = webdriver.ChromeOptions()
-# headless mode: run Chrome in the background
+# Run Chrome in headless mode (in the background)
 options.add_argument("--headless")
-# disable-gpu: disable the GPU hardware acceleration
+# Disable GPU hardware acceleration
 options.add_argument("--disable-gpu")
-# no-sandbox: disable the Chrome sandbox
+# Disable Chrome sandbox
 options.add_argument("--no-sandbox")
-# disable-dev-shm-usage: disable the /dev/shm usage
+# Disable /dev/shm usage
 options.add_argument("--disable-dev-shm-usage")
 
-# Set up new Chrome browser instance
+# Set up the Chrome browser instance
 browser = webdriver.Chrome(service=Service(executable_path=driverPath), options=options)
 browser.maximize_window()
 
@@ -44,7 +49,7 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'
 }
 
-# loop through all the pages, testing for 1 page
+# Loop through all the pages (currently set to 1 page for testing)
 for pageNo in range(1):
     # Send a GET request to the webpage and get the HTML content
     mainPage_URL = f"https://phishtank.org/phish_search.php?page={pageNo}&active=y&valid=y&Search=Search"
@@ -54,7 +59,7 @@ for pageNo in range(1):
     table_present = EC.presence_of_element_located((By.CLASS_NAME, "data"))
     WebDriverWait(browser, 10).until(table_present)
 
-    # Parse the HTML content using BeautifulSoup, driver.page_source is the HTML content of the page
+    # Parse the HTML content using BeautifulSoup
     soup = BeautifulSoup(browser.page_source, "html.parser")
 
     # Find the table that contains the data
@@ -63,7 +68,7 @@ for pageNo in range(1):
     # Get the rows in the table
     rows = table.find_all("tr")
 
-    # Loop row each row in the table
+    # Loop through each row in the table
     for row in rows[1:]:
         cells = row.find_all("td")
 
@@ -74,15 +79,15 @@ for pageNo in range(1):
         phishID_URL = f"https://phishtank.org/phish_detail.php?phish_id={phish_id}"
         browser.get(phishID_URL)
 
-        # Parse the HTML content using BeautifulSoup, driver.page_source is the HTML content of the page
+        # Parse the HTML content using BeautifulSoup
         newSoup = BeautifulSoup(browser.page_source, "html.parser")
 
-        # The required URL of the phishy page is enclosed in a span element with style attribute as 'word-wrap:break-word;'
+        # Find the span element with style attribute 'word-wrap:break-word;'
         spanElement = newSoup.find('span', style='word-wrap:break-word;')
 
         if spanElement is not None:
             requiredElement = spanElement.find('b')
-            
+
             if requiredElement is not None:
                 phishyURL = requiredElement.text.strip()
 
@@ -94,7 +99,6 @@ for pageNo in range(1):
                     nextSoup = BeautifulSoup(HTML_content, 'html.parser')
 
                     # Make a folder with the Phish ID as the folder name to store the HTML content
-                    # folderName is the name of the folder where the HTML content will be stored
                     folderName = f"{phish_id}"
                     # Create the folder if it does not exist
                     os.makedirs(folderName, exist_ok=True)
@@ -103,14 +107,14 @@ for pageNo in range(1):
                     html_filename = os.path.join(folderName, 'index.html')
                     with open(html_filename, "w") as htmlCode:
                         htmlCode.write(nextSoup.prettify())
-                    
+
                     # Find all the Anchor tags
                     anchor_tags = nextSoup.find_all('a')
 
                     # Save anchor tags to file
                     anchor_tags_filename = os.path.join(folderName, 'anchorTags.txt')
                     save_links_to_file([anchor_tag.get('href') for anchor_tag in anchor_tags if anchor_tag.get('href')], anchor_tags_filename)
-                    
+
                     # Find the script tags in the HTML content
                     script_tags = nextSoup.find_all('script')
 
@@ -138,10 +142,10 @@ for pageNo in range(1):
                         download_file(js_file, folderName)
 
                     print(f"Phishy URL {phishyURL} processed successfully.")
-                
+
                 except (requests.RequestException, IOError) as e:
                     print(f"Failed to open URL {phishyURL} due to {e}")
-                
+
     # Go back to the previous page
     browser.back()
     # Wait for 5 seconds before moving to the next page
