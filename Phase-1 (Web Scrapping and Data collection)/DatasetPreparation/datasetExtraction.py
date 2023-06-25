8e1bebd14f358c4bbb9a9a41719876304f010ac0
Shahil Patel <83348636+ShahilPatel-IITDh@users.noreply.github.com>
2023-06-25 15:59:27 +0530

    Updated dataset

--- Phase-1 (Web Scrapping and Data collection)/DatasetPreparation/datasetExtraction.py
+++ Phase-1 (Web Scrapping and Data collection)/DatasetPreparation/datasetExtraction.py
@@ -86,7 +86,7 @@ headers = {
 }
 
 # Loop through the pages of the phishtank website which contains the dataset of URLs
-for pageNo in range(6, 12):
+for pageNo in range(0, 2):
     # Send a GET request to the webpage and get the HTML content
     # Link of phishtank webpage for confirmed Phishy URLs
     mainPage_URL = f"https://phishtank.org/phish_search.php?page={pageNo}&valid=y&Search=Search"
