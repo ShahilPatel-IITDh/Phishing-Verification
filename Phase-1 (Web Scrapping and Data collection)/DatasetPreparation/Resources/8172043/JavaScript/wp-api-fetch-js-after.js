87149d970faf244bea629298ca96a071c3aae9a5
Shahil Patel <83348636+ShahilPatel-IITDh@users.noreply.github.com>
2023-07-17 00:50:03 +0530

    New Dataset

--- Phase-1 (Web Scrapping and Data collection)/DatasetPreparation/Resources/8172043/JavaScript/wp-api-fetch-js-after.js
+++ Phase-1 (Web Scrapping and Data collection)/DatasetPreparation/Resources/8172043/JavaScript/wp-api-fetch-js-after.js
@@ -1,6 +1,6 @@
 
 wp.apiFetch.use( wp.apiFetch.createRootURLMiddleware( "https://www.ebas.ch/wp-json/" ) );
-wp.apiFetch.nonceMiddleware = wp.apiFetch.createNonceMiddleware( "b58efbe306" );
+wp.apiFetch.nonceMiddleware = wp.apiFetch.createNonceMiddleware( "d037286f23" );
 wp.apiFetch.use( wp.apiFetch.nonceMiddleware );
 wp.apiFetch.use( wp.apiFetch.mediaUploadMiddleware );
 wp.apiFetch.nonceEndpoint = "https://www.ebas.ch/wp-admin/admin-ajax.php?action=rest-nonce";
