c0dc764827ba515fc6771b90fbbb03426cf51963
Shahil Patel <83348636+ShahilPatel-IITDh@users.noreply.github.com>
2023-07-17 22:20:46 +0530

    New Dataset

--- Phase-1 (Web Scrapping and Data collection)/DatasetPreparation/Resources/8172017/JavaScript/wp-api-fetch-js-after.js
+++ Phase-1 (Web Scrapping and Data collection)/DatasetPreparation/Resources/8172017/JavaScript/wp-api-fetch-js-after.js
@@ -1,6 +1,6 @@
 
 wp.apiFetch.use( wp.apiFetch.createRootURLMiddleware( "https://www.ebas.ch/wp-json/" ) );
-wp.apiFetch.nonceMiddleware = wp.apiFetch.createNonceMiddleware( "b58efbe306" );
+wp.apiFetch.nonceMiddleware = wp.apiFetch.createNonceMiddleware( "282c1a17f3" );
 wp.apiFetch.use( wp.apiFetch.nonceMiddleware );
 wp.apiFetch.use( wp.apiFetch.mediaUploadMiddleware );
 wp.apiFetch.nonceEndpoint = "https://www.ebas.ch/wp-admin/admin-ajax.php?action=rest-nonce";
--- Phase-1 (Web Scrapping and Data collection)/DatasetPreparation/Resources/8172043/JavaScript/wp-api-fetch-js-after.js
+++ Phase-1 (Web Scrapping and Data collection)/DatasetPreparation/Resources/8172043/JavaScript/wp-api-fetch-js-after.js
@@ -1,6 +1,6 @@
 
 wp.apiFetch.use( wp.apiFetch.createRootURLMiddleware( "https://www.ebas.ch/wp-json/" ) );
-wp.apiFetch.nonceMiddleware = wp.apiFetch.createNonceMiddleware( "d037286f23" );
+wp.apiFetch.nonceMiddleware = wp.apiFetch.createNonceMiddleware( "282c1a17f3" );
 wp.apiFetch.use( wp.apiFetch.nonceMiddleware );
 wp.apiFetch.use( wp.apiFetch.mediaUploadMiddleware );
 wp.apiFetch.nonceEndpoint = "https://www.ebas.ch/wp-admin/admin-ajax.php?action=rest-nonce";
