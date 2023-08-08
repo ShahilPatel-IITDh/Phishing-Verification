9f69332d3acd965bfd2ffce3e0b1c40fb6427ea7
Shahil Patel <83348636+ShahilPatel-IITDh@users.noreply.github.com>
2023-08-08 15:17:52 +0530

    Added Exception handling.

--- Phase-2 (ML classification)/Content based classification/content-features.py
+++ Phase-2 (ML classification)/Content based classification/content-features.py
@@ -15,6 +15,9 @@ list_iFrame = []
 list_form_action = []
 list_favicon_domain = []
 list_enclosed_links = []
+list_max_count_string_matching = []
+list_ratio_max_count_string = []
+list_max_count_string = []
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'
@@ -46,13 +49,13 @@ def requestingURL(url, HTMLPath):
                     file_content = file.read()
 
                     # Find all the image tags present in the file content
-                    image_list.extend(find_tags_in_file(file_content, 'img'))
+                    image_list.append(find_tags_in_file(file_content, 'img'))
 
                     # Find all the video tags present in the file content
-                    video_list.extend(find_tags_in_file(file_content, 'video'))
+                    video_list.append(find_tags_in_file(file_content, 'video'))
 
                     # Find all the audio tags present in the file content
-                    audio_list.extend(find_tags_in_file(file_content, 'audio'))
+                    audio_list.append(find_tags_in_file(file_content, 'audio'))
 
         # Total Number of tags
         total_tags = len(image_list) + len(video_list) + len(audio_list)
@@ -92,9 +95,13 @@ def requestingURL(url, HTMLPath):
             # Phishy
             return -1
 
-    except (Exception, requests.exceptions.HTTPError, requests.exceptions.RequestException) as e:
+    except Exception as e:
         print(f"Error: {e}")
         return 0 
+
+    except requests.exceptions.HTTPError as http_err:
+        print(f"Error: {http_err}")
+        return 0
 
 def extract_a_href_tags_from_file(HTMLPath):
     
@@ -170,9 +177,13 @@ def urlAnchor(url, HTMLPath):
         else:
             return -1
 
-    except (Exception, requests.exceptions.HTTPError, requests.exceptions.RequestException) as e:
+    except (Exception, requests.exceptions.RequestException) as e:
         print(f"Error: {e}")
         return 0  
+
+    except requests.exceptions.HTTPError as http_err:
+        print(f"Error: {http_err}")
+        return 0
 
 def onMouseOver(url, JSPath):
 
@@ -269,8 +280,12 @@ def submitEmail(url, HTMLPath, JSPath):
         # Legitimate - No email submission found in any file
         return 1
 
-    except (Exception, requests.exceptions.HTTPError, requests.exceptions.RequestException) as e:
+    except (Exception, requests.exceptions.RequestException) as e:
         print(f"Error: {e}")
+        return 0
+
+    except requests.exceptions.HTTPError as http_err:
+        print(f"Error: {http_err}")
         return 0
 
 def Check_RightClick(url, JSPath):
@@ -309,8 +324,12 @@ def Check_RightClick(url, JSPath):
         # Right-click not disabled in any file, return 1
         return 1
 
-    except (Exception, requests.exceptions.HTTPError, requests.exceptions.RequestException) as e:
+    except (Exception, requests.exceptions.RequestException) as e:
         print(f"Error: {e}")
+        return 0
+    
+    except requests.exceptions.HTTPError as http_err:
+        print(f"Error: {http_err}")
         return 0
 
 def checkIFrame(url, HTMLPath):
@@ -352,9 +371,13 @@ def checkIFrame(url, HTMLPath):
         # If none of the keywords is found in any HTML file, return 1
         return 1
 
-    except (Exception, requests.exceptions.HTTPError, requests.exceptions.RequestException) as e:
+    except (Exception, requests.exceptions.RequestException) as e:
         print(f"Error: {e}")
-        return -1
+        return 0
+
+    except requests.exceptions.HTTPError as http_err:
+        print(f"Error: {http_err}")
+        return 0
 
 def checkFormAction(url, response, soup):
     domain = url.split('//')[-1].split('/')[0]
@@ -385,15 +408,19 @@ def checkFormAction(url, response, soup):
                 else:
                     return 1
 
-    except (Exception, requests.exceptions.HTTPError, requests.exceptions.RequestException) as e:
+    except (Exception, requests.exceptions.RequestException) as e:
         print(f"Error: Unable to fetch the URL - {e}")
+        return 0
+    
+    except requests.exceptions.HTTPError as http_err:
+        print(f"Error: {http_err}")
         return 0
 
 def checkFaviconDomain(url, response, soup):
 
     try:
         # Fetch the HTML content of the URL
-        response = requests.get(url)
+        # response = requests.get(url)
         response.raise_for_status()
         response_content = response.content
 
@@ -420,9 +447,13 @@ def checkFaviconDomain(url, response, soup):
         else:
             return -1
 
-    except (Exception, requests.exceptions.HTTPError, requests.exceptions.RequestException) as e:
+    except (Exception, requests.exceptions.RequestException) as e:
         print(f"Error: Unable to fetch the URL - {e}")
-        return -99
+        return 0
+    
+    except requests.exceptions.HTTPError as http_err:
+        print(f"Error: {http_err}")
+        return 0
 
 def enclosedLinks(url, HTMLPath, JSPath):
     
@@ -438,6 +469,7 @@ def enclosedLinks(url, HTMLPath, JSPath):
 
     for html_file in html_files:
         file_path = os.path.join(HTMLPath, html_file)
+
         with open(file_path, 'r', encoding='utf-8') as file:
             content = file.read()
 
@@ -487,59 +519,286 @@ def enclosedLinks(url, HTMLPath, JSPath):
         return -1
 
 
+# Below functions will implement the functionality of the 'domain frequency, ratio, string matching etc. function
+def findMaxCount(hrefDomains):
+
+    """
+    Finds the domain string with the maximum occurrence count among the given list of domain strings.
+
+    Parameters:
+        hrefDomains (list): List of domain strings.
+
+    Returns:
+        tuple: Returns a tuple containing the domain string with the maximum occurrence count,
+               the maximum occurrence count, and a required domain string with the maximum count.
+    """
+
+    stringCount_map = {}
+
+    # Count occurence of each string
+    for string in hrefDomains:
+        stringCount_map[string] =  stringCount_map.get(string, 0) + 1
+    
+    # Find the string with the maximum count
+    maxCountString = max(stringCount_map, key=stringCount_map.get) 
+
+    # Find the maximum count
+    maxCount = max(stringCount_map.values()) 
+
+    # Find the string with the maximum count
+    requiredString = max(stringCount_map, key=stringCount_map.get) 
+
+    return maxCountString, maxCount, requiredString
+
+def check_base_domain_against_max_count_domain(url, HTMLPath):
+    """
+    Checks if the base domain of the given URL has the maximum occurrence 
+    among the domains extracted from the 'href' attributes of anchor ('a') tags
+    in an HTML file.
+
+    Parameters:
+        url (str): The URL to be checked.
+        HTMLPath (str): The path to the directory containing the 'landingPage.html' file.
+
+    Returns:
+        int: Returns 1 if the base domain of the URL has the maximum occurrence
+             among the extracted domains, else returns -1. Returns -1 if no 'href' tags
+             are found or if there's an issue with processing.
+    """
+
+    htmlContent = os.path.join(HTMLPath, 'landingPage.html')
+
+    with open(htmlContent, 'r', encoding='utf-8') as file:
+        content = file.read()
+    
+    soup = BeautifulSoup(content, 'html.parser')
+
+    aTags = soup.find_all('a')
+    
+    hrefDomains = []
+
+    if(soup):
+        for link in aTags:
+            if (link):
+                href = link.get('href')
+                if(href):
+                    hrefDomain = urlparse(href).netloc
+
+                    if hrefDomain:
+                        hrefDomains.append(hrefDomain)
+
+    if (len(hrefDomains) != 0):
+        # The findMaxCount function will return 2 values, but in this feature we will use only 1 i.e. maxCountString
+        maxCountString, maxCount, requiredString = findMaxCount(hrefDomains)   
+        parsed_url_base = urlparse(url)
+        baseDomain = parsed_url_base.netloc 
+
+        # Check if the base domain is the maximum occuring domain
+        if(baseDomain == maxCountString):
+            return 1
+
+        else:
+            return -1
+
+    else:
+        # No soup present (phishy)
+        return -1
+
+def calculate_ratio_of_max_domain_occurrence(url, HTMLPath):
+    """
+    Calculates and returns the ratio of the occurrence count of the domain with the maximum 
+    occurrence among the extracted URLs to the total number of URLs.
+
+    Parameters:
+        url (str): The URL associated with the HTML content.
+        HTMLPath (str): The path to the directory containing the 'landingPage.html' file.
+
+    Returns:
+        int: Returns the ratio of the maximum occurrence domain to the total number of URLs.
+             Returns -99 if there is an issue with processing or if the maxCount is None.
+             Returns 0 if there are no URLs.
+    """
+    htmlContent = os.path.join(HTMLPath, 'landingPage.html')
+
+    with open(htmlContent, 'r', encoding='utf-8') as file:
+        content = file.read()
+
+    soup = BeautifulSoup(content, 'html.parser')
+
+    aTags = soup.find_all('a')
+    totalHref = []
+
+    for link in aTags:
+        href = link.get('href')
+        if href:
+            totalHref.append(href)
+
+    if len(totalHref) != 0:
+        maxCountString, maxCount, requiredString = findMaxCount(totalHref) 
+
+        if maxCount is not None:
+            return int(maxCount / len(totalHref))
+        else:
+            return -99
+
+    else:
+        return 0  # Return appropriate value if totalHref is empty
+
+def check_max_domain_occurrence_in_related_elements(url, HTMLPath):
+    """
+    Determines whether the base domain of the given URL matches the domain with the maximum
+    occurrence among the domains extracted from 'link', 'img', and 'script' elements in an HTML file.
+
+    Parameters:
+        url (str): The URL associated with the HTML content.
+        HTMLPath (str): The path to the directory containing the 'landingPage.html' file.
+
+    Returns:
+        int: Returns 1 if the base domain matches the domain with the maximum occurrence among extracted domains.
+             Returns -1 if the base domain does not match or there is an issue with processing.
+             Returns 0 if there's an exception or error while processing.
+    """
+
+    htmlContent = os.path.join(HTMLPath, 'landingPage.html')
+
+    with open(htmlContent, 'r', encoding='utf-8') as file:
+        content = file.read()
+    
+    soup = BeautifulSoup(content, 'html.parser')
+
+    try:
+        all_links = []
+        if soup.text:
+            links = soup.find_all("link")
+            if links:
+                for link in links:
+                    the_link = link.get('href')
+                    if the_link: 
+                        parsed_url_link = urlparse(the_link)
+                        domain_link = parsed_url_link.netloc
+                        if(domain_link):
+                            all_links.append(domain_link)
+            
+            images = soup.find_all("img")
+
+            if images:
+                for i in images:
+                    check_url = i.get('src')
+                    if check_url:
+                        if "http" in check_url or "https" in check_url or "HTTP" in check_url or "HTTPS" in check_url:
+                            parsed_url_images = urlparse(check_url)
+                            domain_images = parsed_url_images.netloc
+                            if(domain_images):
+                                all_links.append(domain_images)           
+
+            scripts = soup.find_all("script")
+                
+            if scripts:
+                for script in scripts:
+                    src = script.get('src')
+                    if src:
+                        parsed_url_src = urlparse(src)
+                        domain_src = parsed_url_src.netloc
+                        if(domain_src):
+                            all_links.append(domain_src)  
+            
+            if len(all_links) != 0:
+                    maxCountString, maxCount, requiredString = findMaxCount(all_links) 
+                    parsed_url_base = urlparse(url)
+                    base_domain = parsed_url_base.netloc 
+
+                    if(requiredString != base_domain):
+                        return -1
+                    
+                    else:
+                        return 1
+        else:
+            return -1
+    
+    except (Exception, requests.exceptions.HTTPError) as http_err:
+        return 0
+
 def beginProcess(url, phishID):
 
-    # Folder paths for HTML, JS content
+    # Folder paths for HTML, JS content (For the Legitimate ones)
     HTMLPath = f"/home/administrator/Desktop/Phishing-Verification/Phase-1 (Web Scrapping and Data collection)/DatasetPreparation/Legitimate-Resources/{phishID}/HTML/"
 
     JSPath = f"/home/administrator/Desktop/Phishing-Verification/Phase-1 (Web Scrapping and Data collection)/DatasetPreparation/Legitimate-Resources/{phishID}/JavaScript/"
+
+    # HTMLPath = f"/home/administrator/Desktop/Phishing-Verification/Phase-1 (Web Scrapping and Data collection)/DatasetPreparation/Phishy-Resources/{phishID}/HTML/"    
+
+    # JSPath = f"/home/administrator/Desktop/Phishing-Verification/Phase-1 (Web Scrapping and Data collection)/DatasetPreparation/Phishy-Resources/{phishID}/JavaScript/"
 
     # 0: Append the url also in the csv file
     # list_url.append(url)
 
     # Fetch the HTML content of the URL
-    response = requests.get(url, headers=headers)
-    response.raise_for_status()
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
 
-    # Parse the HTML content using BeautifulSoup
-    soup = BeautifulSoup(response.content, 'html.parser')
+        # Parse the HTML content using BeautifulSoup
+        soup = BeautifulSoup(response.content, 'html.parser')
 
-    # 1: Request the url
-    requestURL = int(requestingURL(url, HTMLPath))
-    list_request.append(requestURL)
+        # 1: Request the url
+        requestURL = int(requestingURL(url, HTMLPath))
+        list_request.append(requestURL)
 
-    # 2: URL of anchor
-    URL_anchor = int(urlAnchor(url, HTMLPath))
-    list_anchor.append(URL_anchor)
+        # 2: URL of anchor
+        URL_anchor = int(urlAnchor(url, HTMLPath))
+        list_anchor.append(URL_anchor)
 
-    # 3: Submitting to Mail
-    submit_to_mail = int(submitEmail(url, HTMLPath, JSPath))
-    list_email.append(submit_to_mail)
+        # 3: Submitting to Mail
+        submit_to_mail = int(submitEmail(url, HTMLPath, JSPath))
+        list_email.append(submit_to_mail)
 
-    # 4: On Mouseover
-    on_mouse_over = int(onMouseOver(url, JSPath))
-    list_mouse_over.append(on_mouse_over)
+        # 4: On Mouseover
+        on_mouse_over = int(onMouseOver(url, JSPath))
+        list_mouse_over.append(on_mouse_over)
 
-    # 5: Right Click
-    right_click = int(Check_RightClick(url, JSPath))
-    list_right_click.append(right_click)
+        # 5: Right Click
+        right_click = int(Check_RightClick(url, JSPath))
+        list_right_click.append(right_click)
 
-    # 6: IFrame
-    iFrame = int(checkIFrame(url, HTMLPath))
-    list_iFrame.append(iFrame)
+        # 6: IFrame
+        iFrame = int(checkIFrame(url, HTMLPath))
+        list_iFrame.append(iFrame)
 
-    # 7: Form Action
-    form_action = int(checkFormAction(url, response, soup))
-    list_form_action.append(form_action)
+        # 7: Form Action
+        form_action = int(checkFormAction(url, response, soup))
+        list_form_action.append(form_action)
 
-    # 8: Favicon domain check
-    favicon_domain = int(checkFaviconDomain(url, response, soup))
-    list_favicon_domain.append(favicon_domain)
+        # 8: Favicon domain check
+        favicon_domain = int(checkFaviconDomain(url, response, soup))
+        list_favicon_domain.append(favicon_domain)
 
-    # 9: Links in Meta, link and script tags
-    enclosed_links = int(enclosedLinks(url, HTMLPath, JSPath))
-    list_enclosed_links.append(enclosed_links)
+        # 9: Links in Meta, link and script tags
+        enclosed_links = int(enclosedLinks(url, HTMLPath, JSPath))
+        list_enclosed_links.append(enclosed_links)
 
+        # 10: Max count string
+        maxCountString_matching = int(check_base_domain_against_max_count_domain(url, HTMLPath))
+        list_max_count_string_matching.append(maxCountString_matching)
+
+        # 11: Ratio of maxCountString to total number of strings
+        ratio_maxCountString = int(calculate_ratio_of_max_domain_occurrence(url, HTMLPath))
+        list_ratio_max_count_string.append(ratio_maxCountString)
+
+        # 12: String with maximum Count (same as feature-10, buts checks in links, images, scripts, etc. tags)
+        maxCountString = int(check_max_domain_occurrence_in_related_elements(url, HTMLPath))
+        list_max_count_string.append(maxCountString)
+    
+    except requests.exceptions.HTTPError as http_err:
+        if response.status_code == 404:
+            print("------------------------")
+            print(f"HTTPError: {response.status_code} - Not Found: {url}")
+        else:
+            print("------------------------")
+            print(f"HTTPError: {response.status_code} - An error occurred: {http_err}")
+
+    except Exception as e:
+        print("---------------------------")
+        print(f"An error occurred: {e}")
 
 def generateCSV():
 
@@ -553,7 +812,10 @@ def generateCSV():
         'IFrame': list_iFrame,
         'Form Action': list_form_action,
         'Favicon Domain': list_favicon_domain,
-        'Enclosed Links': list_enclosed_links
+        'Enclosed Links': list_enclosed_links,
+        'Max Count String Matching': list_max_count_string_matching,
+        'Ratio Max Count String': list_ratio_max_count_string,
+        'String with Maximum Count': list_max_count_string
     }
 
     # Create a DataFrame from the dictionary
@@ -596,8 +858,11 @@ if __name__ == '__main__':
 
                 print(count)
                 count+=1
-
+                if count<100:
                 # count limit added just for checking the code on a small set of URLs
-                beginProcess(URL, phishID)
+                    beginProcess(URL, phishID)
+                
+                else:
+                    break
 
         generateCSV()
