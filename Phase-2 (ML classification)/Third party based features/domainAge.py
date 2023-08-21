1c3678daa0ceb6e647b56e8326b3de5c38818598
Shahil Patel <83348636+ShahilPatel-IITDh@users.noreply.github.com>
2023-08-21 22:59:09 +0530

    Removed Unwanted code

--- Phase-2 (ML classification)/Third party based features/domainAge.py
+++
@@ -1,8 +0,0 @@
-from urllib.parse import urlparse
-import whois
-
-input_website = "https://www.instagram.com"
-parsedURL = urlparse(input_website)
-domain = parsedURL.netloc
-whoisInstance = whois.whois(domain)
-print(whoisInstance)
