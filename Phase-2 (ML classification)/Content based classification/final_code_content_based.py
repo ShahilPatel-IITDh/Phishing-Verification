57a0ce399b091f45790bbbd29fb9255c5068f59e
Shahil Patel <83348636+ShahilPatel-IITDh@users.noreply.github.com>
2023-08-24 06:11:21 +0530

    Final working code for content based feature

--- Phase-2 (ML classification)/Content based classification/final_code_content_based.py
+++ Phase-2 (ML classification)/Content based classification/final_code_content_based.py
@@ -7,7 +7,7 @@ from urllib.parse import urlparse
 import re
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'
 }
 
 one = []
@@ -33,8 +33,8 @@ def generateCSV():
         'presence_of_iframes': five,
         'presence_of_popups' : six,
         'right_click_disabling' : seven,
-        'num_of_redirects' : eight,
-        'checking_cookies': nine,
+        # 'num_of_redirects' : eight,
+        # 'checking_cookies': nine,
         'checking_sfh': ten, 
         'request_url': eleven,
         'url-of-anchor': twelve,
@@ -45,10 +45,10 @@ def generateCSV():
     df = pd.DataFrame(data)
 
     # Write the Phishy DataFrame to a Phishy-Data CSV file
-    df.to_csv('Phishy-Data.csv', index=False)
+    # df.to_csv('Phishy-Data.csv', index=False)
 
     # Write the Legitimate DataFrame to a Legitimate-Data CSV file
-    # df.to_csv('Legitimate-Content-Data.csv', index=False)
+    df.to_csv('Legitimate-Content-Data.csv', index=False)
 
     
 
@@ -85,13 +85,13 @@ def find_max_count(strings_list):
 
     # with open("max_url-count.txt","a") as f3:
     #     if(max_count_string):
-    #         # f3.write(f"{max_count_string}\n") 
+    #         f3.write(f"{max_count_string}\n") 
 
-    #     else:
-    #         f3.write("no highest one\n")
-    #         f3.write(f"{string_count_map}\n")
+        # else:
+        #     f3.write("no highest one\n")
+        #     f3.write(f"{string_count_map}\n")
         
-    #     # f3.write("-----------------------------")
+        # f3.write("-----------------------------")
 
     return max_count_string
 
@@ -99,58 +99,83 @@ def frequency_alltags(soup,url):
 
     all_links = [] 
 
-    if soup.text:
-                links = soup.find_all("link")
-                if links:
-                    for link in links:
-                        the_link = link.get('href')
-                        if the_link: 
-                            parsed_url_link = urlparse(the_link)
-                            domain_link = parsed_url_link.netloc
-                            if(domain_link):
-                                all_links.append(domain_link)
+    if soup:
+        links = soup.find_all("link")
+        if links:
+            for link in links:
+                the_link = link.get('href')
+                if the_link: 
+                    parsed_url_link = urlparse(the_link)
+                    domain_link = parsed_url_link.netloc
+                    if(domain_link):
+                        all_links.append(domain_link) 
+                    else:
+                        return 0    
+                else:
+                    return 0        
+        else:
+            return 0                
                             
 
-                images = soup.find_all("img")
+        images = soup.find_all("img")
 
-                if images:
-                    for i in images:
-                        check_url = i.get('src')
-                        if check_url:
-                            if "http" in check_url or "https" in check_url or "HTTP" in check_url or "HTTPS" in check_url:
-                                parsed_url_images = urlparse(check_url)
-                                domain_images = parsed_url_images.netloc
-                                if(domain_images):
-                                    all_links.append(domain_images)
+        if images:
+            for i in images:
+                check_url = i.get('src')
+                if check_url:
+                    if "http" in check_url or "https" in check_url or "HTTP" in check_url or "HTTPS" in check_url:
+                        parsed_url_images = urlparse(check_url)
+                        domain_images = parsed_url_images.netloc
+                        if(domain_images):
+                            all_links.append(domain_images) 
+                        else:
+                            return 0    
+                    else:
+                        return 0        
+                else:
+                    return 0            
+        else:
+            return 0                    
                        
 
-                scripts = soup.find_all("script")
+        scripts = soup.find_all("script")
                 
-                if scripts:
-                    for script in scripts:
-                        src = script.get('src')
-                        if src:
-                            parsed_url_src = urlparse(src)
-                            domain_src = parsed_url_src.netloc
-                            if(domain_src):
-                                all_links.append(domain_src)  
+        if scripts:
+            for script in scripts:
+                src = script.get('src')
+                if src:
+                    parsed_url_src = urlparse(src)
+                    domain_src = parsed_url_src.netloc
+                    if(domain_src):
+                        all_links.append(domain_src)  
+                    else:
+                        return 0    
+                else:
+                    return 0        
+        else:
+            return 0                
 
 
-                if len(all_links) != 0:
-                    max_count_string = find_max_count(all_links) 
-                    parsed_url_base = urlparse(url)
-                    base_domain = parsed_url_base.netloc  
+        if len(all_links) != 0:
+            max_count_string = find_max_count(all_links) 
+            parsed_url_base = urlparse(url)
+            base_domain = parsed_url_base.netloc  
 
-                    # with open("compare.txt","a") as f1:
-                    #     f1.write(f"{max_count_string} ")
-                    #     f1.write(f"{base_domain}\n")
-                    #     f1.write("-------------------------")
+            # with open("compare.txt","a") as f1:
+            #     f1.write(f"{max_count_string} ")
+            #     f1.write(f"{base_domain}\n")
+            #     f1.write("-------------------------")
 
-                    with open("final_result.txt",'a') as f:
-                        if(max_count_string!=base_domain):
-                            return -1
-                        else:
-                            return 1 
+            if(max_count_string!=base_domain):
+                    return -1
+            else:
+                    return 1 
+             
+        else:
+            return 0  
+         
+    else:
+        return 0          
 
 
 def frequency_atags(soup,url):
@@ -159,25 +184,33 @@ def frequency_atags(soup,url):
 
     if soup:
         a_tags = soup.find_all('a')
-        for link in a_tags:
-            if link:
-                
-                href = link.get('href')
-                if href:
-                    parsed_href = urlparse(href)
-                    domain_href = parsed_href.netloc 
-                    if domain_href:  # Check if the domain is not empty before appending
-                        hrefs_domain.append(domain_href)
+        if(a_tags):
+            for link in a_tags:
+                if link:
+                    href = link.get('href')
+                    if href:
+                        parsed_href = urlparse(href)
+                        domain_href = parsed_href.netloc 
+                        if domain_href:  # Check if the domain is not empty before appending
+                            hrefs_domain.append(domain_href)
 
-    if len(hrefs_domain) != 0:
-        max_count_string = find_max_count_string(hrefs_domain) 
-        parsed_url_base = urlparse(url)
-        base_domain = parsed_url_base.netloc
+            if len(hrefs_domain) != 0:
+                max_count_string = find_max_count_string(hrefs_domain) 
+                parsed_url_base = urlparse(url)
+                base_domain = parsed_url_base.netloc
 
-        if(base_domain == max_count_string):
-            return 1 #legitimate
+                if(base_domain == max_count_string):
+                    return 1 #legitimate
+                else:
+                    return -1  #phishy 
+            else:
+                return 0 
+            
         else:
-            return -1  #phishy
+            return 0       
+
+    else:
+        return 0    
 
 def check_iframes(soup,url):
 
@@ -188,21 +221,23 @@ def check_iframes(soup,url):
 
 def check_popup(soup,url):
 
-    script_tags = soup.find_all('script')
-
-# Loop through the script tags
-    for script_tag in script_tags:
-        script_content = script_tag.string 
-
-        # with open("script_content","a") as f3:
-        #     f3.write(f"{script_content}")
-        #     f3.write("-------------------")
-
-        if script_content:
-            if re.search(r"alert\(|confirm\(", script_content):
-                return -1
-            else:
-                return 1 
+    if(soup):
+        script_tags = soup.find_all('script')
+        #Loop through the script tags
+        if(script_tags):
+            for script_tag in script_tags:
+                script_content = script_tag.string 
+                if script_content:
+                    if re.search(r"alert\(|confirm\(", script_content):
+                        return -1
+                    else:
+                        return 1 
+                else:
+                    return 0     
+        else:
+            return 0 ## to fill none values   
+    else:
+        return 0    ##no soup present       
             
 def right_click_disabled(soup,url):
 
@@ -226,6 +261,9 @@ def redirects(soup,url):
                return 0
            else:
                return -1
+        else:
+            return 0
+           
     except requests.exceptions.RequestException as e:
         return -99
 
@@ -270,9 +308,11 @@ def processing_on_links(url, img, vid, aud): ##helper function
 
     parsed_url = urlparse(url)
     domain = parsed_url.netloc 
+    # print("the domain is: ",domain)
 
     domain_without_extra = domain.replace("www.","")
-    domain_without_extra = domain_without_extra.replace(".com","")
+    domain_without_extra = domain_without_extra.replace(".com","") 
+    # print("removing extra:-",domain_without_extra)
 
     count_outer_domain = 0 
 
@@ -291,8 +331,10 @@ def processing_on_links(url, img, vid, aud): ##helper function
         if domain not in link and (domain_without_extra not in link):
             count_outer_domain += 1                
 
+    # print(f"Count of outer domain links: {count_outer_domain}")  
+    # print(f"Total number of links: {total_length}")      
 
-    # check validity 
+    ##check validity 
     percentage = (count_outer_domain/total_length)*(100)
 
     # with open("percentages.txt","a") as f:
@@ -315,7 +357,6 @@ def request_url(soup,url):
     audio_list = [] 
 
     if(soup):
-
         img_tags = soup.find_all('img')  # Find all <img> tags 
 
         for tag in img_tags:
@@ -341,9 +382,9 @@ def request_url(soup,url):
         return returned_result
 
     else:
-        print("no soup present")  
+        print("no soup present")
 
-def processing_on_anchors(a_hrefs,url): #helper function
+def processing_on_anchors(a_hrefs, url):
 
     total_length= len(a_hrefs)
 
@@ -352,11 +393,9 @@ def processing_on_anchors(a_hrefs,url): #helper function
 
     parsed_url = urlparse(url)
     domain = parsed_url.netloc 
-    print("the domain is: ",domain) 
 
     domain_without_extra = domain.replace("www.","")
     domain_without_extra = domain_without_extra.replace(".com","") 
-    print("removing extra:-",domain_without_extra)  
 
     count_outer_domain = 0
 
@@ -370,12 +409,14 @@ def processing_on_anchors(a_hrefs,url): #helper function
 
     percentage = (count_outer_domain/total_length)*100 
     
-    print(percentage)
+    # print(percentage)
 
     if(percentage<31):
         return 1
+
     elif(percentage>=31 and percentage<=67):
         return 0  
+
     else:
         return -1                   
 
@@ -397,6 +438,7 @@ def url_of_anchor(soup,url):
         return classification_result
 
 def links_in_general(soup,url):
+
     parsed_url = urlparse(url)
     domain = parsed_url.netloc  
     just_main = domain.replace("www.", "")
@@ -437,7 +479,6 @@ def links_in_general(soup,url):
 
         for addy in links_href:
             if domain not in addy  and addy[0] != '/':
-                # print("the addy is:", addy)
                 unsafe += 1
 
         for addy in meta_content:
@@ -447,7 +488,8 @@ def links_in_general(soup,url):
         for addy in script_src:
             if domain not in addy and addy[0] != '/':
                 unsafe += 1 
-     
+
+        # print("unsafe count is:-",unsafe)        
 
         percentage = (unsafe / total_length) * 100 
 
@@ -464,84 +506,80 @@ def links_in_general(soup,url):
 
 
 # Define a function for processing HTML content
-def begin_processing(phishid, url, count):
+def begin_processing(phishid, url):
 
 
-    # for phishid, url in zip(phishid_list,url_list):
+        
+    # Legitimate HTML file path
+    html_file_path = f"/home/administrator/Desktop/Phishing-Verification/Phase-1 (Web Scrapping and Data collection)/DatasetPreparation/Legitimate-Resources/{phishid}/HTML/landingPage.html"
 
-    # Legitimate HTML files
-    # html_file_path = f"/home/administrator/Desktop/Phishing-Verification/Phase-1 (Web Scrapping and Data collection)/DatasetPreparation/Legitimate-Resources/{phishid}/HTML/landingPage.html"
+    # Phishy HTML file path
+    # html_file_path = '/home/administrator/Desktop/Phishing-Verification/Phase-1 (Web Scrapping and Data collection)/DatasetPreparation/Phishy-Resources/{phishid}/HTML/landingPage.html'
 
-    # Phishy HTML files
-    html_file_path = f"/home/administrator/Desktop/Phishing-Verification/Phase-1 (Web Scrapping and Data collection)/DatasetPreparation/Phishy-Resources/{phishid}/HTML/landingPage.html"
+    with open(html_file_path, 'r') as f:
+        html_content = f.read()
 
-    if os.path.exists(html_file_path):
-        with open(html_file_path, 'r') as f:
-            html_content = f.read()
+    soup = BeautifulSoup(html_content, 'html.parser')
 
-        soup = BeautifulSoup(html_content, 'html.parser')
-
-        # write all the functions from here and pass the soup here
-        #1st content feature
-        c_f1 = frequency_atags(soup,url) 
-        one.append(c_f1) 
-
-        c_f3 = frequency_alltags(soup,url)
-        three.append(c_f3) 
-            
-        c_f5 = check_iframes(soup,url)
-        five.append(c_f5)
-            
-        c_f6 = check_popup(soup,url)
-        six.append(c_f6)
-            
-        c_f7 = right_click_disabled(soup,url)
-        seven.append(c_f7)
-            
-        c_f8 = redirects(soup,url)
-        eight.append(c_f8) 
-            
-        c_f9 = check_cookies(soup,url)
-        nine.append(c_f9) 
-            
-        c_10 = check_sfh(soup,url)
-        ten.append(c_10) 
-            
-        c_11 = request_url(soup,url)
-        eleven.append(c_11)
-            
-        c_12 = url_of_anchor(soup,url)
-        twelve.append(c_12)
-            
-        c_13 = links_in_general(soup,url)
-        thirteen.append(c_13)
-            
-        print(f"{count}\n")
-        print("------------------------------------------")
+    # write all the functions from here and pass the soup here
+    #1st content feature
     
-    else:
-        print(f"HTML file not found for phishid: {phishid}")
+    c_f1 = frequency_atags(soup,url) 
+    one.append(c_f1) 
+
+    c_f3 = frequency_alltags(soup,url)
+    three.append(c_f3) 
+        
+    c_f5 = check_iframes(soup,url)
+    five.append(c_f5)
+        
+    c_f6 = check_popup(soup,url)
+    six.append(c_f6)
+        
+    c_f7 = right_click_disabled(soup,url)
+    seven.append(c_f7)
+        
+    # c_f8 = redirects(soup,url)
+    # eight.append(c_f8) 
+        
+    # c_f9 = check_cookies(soup,url)
+    # nine.append(c_f9) 
+        
+    c_10 = check_sfh(soup,url)
+    ten.append(c_10) 
+        
+    c_11 = request_url(soup,url)
+    eleven.append(c_11)
+        
+    c_12 = url_of_anchor(soup,url)
+    twelve.append(c_12)
+        
+    c_13 = links_in_general(soup,url)
+    thirteen.append(c_13)
+        
 
 if __name__ == "__main__":
     
     # Legitimate Excel file
-    # ExcelFilePath = '/home/administrator/Desktop/Phishing-Verification/Phase-1 (Web Scrapping and Data collection)/DatasetPreparation/Legitimate-Data.xlsx'
+    ExcelFilePath = '/home/administrator/Desktop/Phishing-Verification/Phase-1 (Web Scrapping and Data collection)/DatasetPreparation/Legitimate-Data.xlsx'
 
     # Legitimate Folder Path
-    # resources_folder_path = '/home/administrator/Desktop/Phishing-Verification/Phase-1 (Web Scrapping and Data collection)/DatasetPreparation/Legitimate-Resources/'
+    resources_folder_path = '/home/administrator/Desktop/Phishing-Verification/Phase-1 (Web Scrapping and Data collection)/DatasetPreparation/Legitimate-Resources/'
 
     # Phishy Excel file
-    ExcelFilePath = '/home/administrator/Desktop/Phishing-Verification/Phase-1 (Web Scrapping and Data collection)/DatasetPreparation/Phishy-Data.xlsx'
+    # ExcelFilePath = '/home/administrator/Desktop/Phishing-Verification/Phase-1 (Web Scrapping and Data collection)/DatasetPreparation/Phishy-Data.xlsx'
 
     # Phishy Folder Path
-    resources_folder_path = '/home/administrator/Desktop/Phishing-Verification/Phase-1 (Web Scrapping and Data collection)/DatasetPreparation/Phishy-Resources/'
+    # resources_folder_path = '/home/administrator/Desktop/Phishing-Verification/Phase-1 (Web Scrapping and Data collection)/DatasetPreparation/Phishy-Resources/'
 
     # Load the Excel file using pandas
     df = pd.read_excel(ExcelFilePath)
+    # print(df) 
+    # print("the 6th feature_list is:-",six)   
 
-    output_folder_path = '/home/administrator/Desktop/Phishing-Verification/Phase-2 (ML classification)/Content based classification/'
+    # output_folder_path = '/home/administrator/Desktop/Phishing-Verification/Phase-2 (ML classification)/Content based classification/'
 
-    os.makedirs(output_folder_path, exist_ok=True)
+    # os.makedirs(output_folder_path, exist_ok=True)
 
     phishid_list = []
 
@@ -549,24 +587,17 @@ if __name__ == "__main__":
     count = 1
 
     for index, row in df.iterrows():
-        if(count<=800):
 
-            phishid = str(row['PhishID'])
-            check_html = row['HTML'] 
-            check_url = row['URL']
-            phishid_folder_path = os.path.join(resources_folder_path, phishid, 'HTML')
 
-            # if os.path.exists(phishid_folder_path) and check_html == 1:
-            #     phishid_list.append(phishid)
-            #     urls.append(check_url)
-        
-            # begin_processing(phishid_list, urls)
-            # generateCSV()
+        phishid = str(row['PhishID'])
+        check_html = row['HTML'] 
+        check_url = row['URL']
 
-            begin_processing(phishid, check_url, count)
+        phishid_folder_path = os.path.join(resources_folder_path, phishid, 'HTML')
+
+        if os.path.exists(phishid_folder_path) and check_html == 1:
+            begin_processing(phishid, check_url)
             generateCSV()
-
-            count+=1
-
-        else:
-            break
+        
+        count+=1
+        print(count)
