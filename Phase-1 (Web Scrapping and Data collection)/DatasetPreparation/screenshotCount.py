61381d796ee2f5f03a27dda08b8c6513569b15cb
Shahil Patel <83348636+ShahilPatel-IITDh@users.noreply.github.com>
2023-10-22 11:29:29 +0530

    New screenshots

--- Phase-1 (Web Scrapping and Data collection)/DatasetPreparation/screenshotCount.py
+++ Phase-1 (Web Scrapping and Data collection)/DatasetPreparation/screenshotCount.py
@@ -6,42 +6,60 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import WebDriverException
 import time
+import os
+from PIL import Image
+import io
 
 # Read the URLs from the Excel file
 xlsx_file_path = 'Legitimate-Data.xlsx' 
-output_file = 'screenshotCount.txt'
+output_file = 'screenshotCount-Legitimate.txt'
 
 # Load the Excel file
 try:
-    df = pd.read_excel(xlsx_file_path, sheet_name=sheet_name)
+    df = pd.read_excel(xlsx_file_path)
     urls_phishid_data = df[['URL', 'PhishID']].values.tolist()
 
 except Exception as e:
     print(f"Error reading the Excel file: {str(e)}")
     urls_phishid_data = []
 
-# Initialize the Chrome WebDriver
-chrome_options = Options()
-chrome_options.headless = True  # Run Chrome in headless mode (no GUI)
-# service = Service('/path/to/chromedriver')
-# driver = webdriver.Chrome(service=service, options=chrome_options)
-driver = webdriver.Chrome()
+# driver path (chrome driver), in ubuntu
+driverPath = "/home/administrator/Downloads/chromedriver_linux64/chromedriver"
+
+# Use webdriver-manager to automatically download the ChromeDriver binary
+# service = Service("webdriver-manager/chromedriver.exe")
+
+# create a new Chrome browser instance
+options = webdriver.ChromeOptions()
+# headless mode: run Chrome in the background
+options.add_argument("--headless")
+# disable-gpu: disable the GPU hardware acceleration
+options.add_argument("--disable-gpu")
+# no-sandbox: disable the Chrome sandbox
+options.add_argument("--no-sandbox")
+# disable-dev-shm-usage: disable the /dev/shm usage
+options.add_argument("--disable-dev-shm-usage")
+
+# Set up new Chrome browser instance
+driver = webdriver.Chrome(service=Service(executable_path=driverPath), options=options)
+driver.maximize_window()
 
 # Initialize counters
 success_count = 0
 failure_count = 0
 
+# Define the directory path
+screenshot_directory = "Screenshots_Using_PIL"
+
+URLCount = 0
+
 # Loop through the URLs and take screenshots
 for url, phishID in urls_phishid_data:
     try:
         driver.get(url)
-        time.sleep(10)  # Wait for 10 seconds for the page to load
-
-        # # Capture a screenshot
-        # screenshot_filename = f'screenshot_{success_count + 1}.png'
-        # driver.save_screenshot(screenshot_filename)
-        # print(f'Screenshot captured for {url}')
+        time.sleep(3)  # Wait for 10 seconds for the page to load
 
         # Take a screenshot of the visible portion of the website
         screenshot = driver.get_screenshot_as_png()
@@ -49,18 +67,24 @@ for url, phishID in urls_phishid_data:
         image = Image.open(io.BytesIO(screenshot))
 
         # Save the screenshot to a file (e.g., "screenshot.png")
-
-        screenshotFile = f"{phishID}_screenshot.png"
-
-        # filePath = os.path.join(ScreenShot_Directory, screenshotFile)
-        image.save(filePath)
-
+# Save the screenshot to the directory
+        screenshotFile = os.path.join(screenshot_directory, f"{phishID}_screenshot.png")
+        os.makedirs(screenshot_directory, exist_ok=True)
+        image.save(screenshotFile)
 
         success_count += 1
+        print(f'{phishID} processed \n')
         
     except Exception as e:
         print(f'Error capturing screenshot for {url}: {str(e)}')
         failure_count += 1
+
+        print(f'{phishID} not processed \n')
+    
+    URLCount+=1
+
+    if(URLCount == 50):
+        break
 
 # Close the Chrome WebDriver
 driver.quit()
